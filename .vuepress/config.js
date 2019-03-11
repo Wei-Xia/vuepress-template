d7fb1aaad69816102a687179fea986ef54b1d37d
Wei Xia <xw900812@gmail.com>
2019-03-11 15:34:32 -0400

    initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	base: '/w3-notifications/notifications-doc-site/',
+	title: 'Notifications Documentation',
+	dest: './docs',
+	themeConfig: {
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Single Page", link: "/singlePage/" },
+			{ text: "Multiple Pages", link: "/multiplePages/" },
+		],
+		sidebar: {
+			'/multiplePages/': genSidebarConfigDesign('Multiple Pages')
+		},
+		lastUpdated: 'Last Updated',
+		displayAllHeaders: true,
+		repo: 'https://github.ibm.com/w3-notifications/notifications-doc-site/',
+		repoLabel: 'Contribute!',
+		docsRepo: 'https://github.ibm.com/w3-notifications/notifications-doc-site/',
+		docsBranch: 'master',
+		editLinks: true,
+		editLinkText: 'Help us improve this page!'
+	},
+};
+
+function genSidebarConfigDesign (title) {
+	return [
+		{
+			title,
+			collapsable: false,
+			children: [
+				'',
+				'pageOne',
+				'pageTwo',
+			]
+		}
+	]
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+# VuePress Github Pages
+
+Use [VuePress](https://vuepress.vuejs.org/) to built a static website and deploy on [Github Pages](https://pages.github.com/).
+
+## Demo
+
+See this page on https://pages.github.ibm.com/w3-notifications/notifications-doc-site/
+
+## Start
+
+Make sure that you have NodeJS and NPM or Yarn installed.
+
+## Install
+
+```sh
+npm install
+```
+
+or
+
+```sh
+yarn
+```
+
+## Develop
+
+```sh
+npm run start
+```
+
+or
+
+```sh
+yarn start
+```
+
+## Build
+
+```sh
+npm run build
+```
+
+or
+
+```sh
+yarn build
+```
